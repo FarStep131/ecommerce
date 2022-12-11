d07229e2649a914ab4d81aafb5c2f99d0bbae6c0
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-12-11 12:00:41 +0900

    Change redirect destination after login using after_sign_in_path_for

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,10 +18,14 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(_resource)
+    admin_root_path
+  end
 end
