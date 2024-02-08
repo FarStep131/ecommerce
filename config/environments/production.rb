6842c467cc03fb9ddaf1815a433d7b49216b0b9b
naoki-yazawa <naoki.yazawa@dena.com>
2024-02-08 14:50:14 +0900

    feat: modify config.action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.default_url_options = { host: 'web-tusj.onrender.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'web-tusj.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
